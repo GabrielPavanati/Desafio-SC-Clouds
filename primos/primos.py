b4d9f11a0cbf90fff85e588319d21e18b4a5eba7
Gabriel Furtado Pavanati <gpavanati01@gmail.com>
2023-08-31 11:10:47 -0300

    add input and print to primos.py file

--- primos/primos.py
+++ primos/primos.py
@@ -3,11 +3,23 @@
 # Trata input para receber apenas números maiores que 1
 while True:
     try:
-        print("Informe um número maior que 1: ")
-        N = int(input())
+        N = int(input("Informe um número maior que 1: "))
 
     except:
         continue
     
     if N > 1:
         break
+
+print("p_linear({}) = {}".format(N, p_linear(N))) # Imprime todos os números primos até o número N através da função linear
+print("p_recursivo({}) = {}".format(N, p_recursivo(N))) # Imprime todos os números primos até o número N através da função recursiva
+
+# Função que calcula todos os números primos até o número N de forma linear
+def p_linear(N):
+    # código
+    return
+
+# Função que calcula todos os números primos até o número N de forma recursiva
+def p_recursivo(N):
+    # código
+    return
