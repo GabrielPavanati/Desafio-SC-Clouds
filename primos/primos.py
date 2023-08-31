4c5e50f8333b66bef711685892c7adc3c0fb827f
Gabriel Furtado Pavanati <gpavanati01@gmail.com>
2023-08-31 11:22:55 -0300

    rearrange primos.py code

--- primos/primos.py
+++ primos/primos.py
@@ -1,5 +1,15 @@
 # coding: latin-1
 
+# Função que calcula todos os números primos até o número N de forma linear
+def p_linear(N):
+    # código
+    return
+
+# Função que calcula todos os números primos até o número N de forma recursiva
+def p_recursivo(N):
+    # código
+    return
+
 # Trata input para receber apenas números maiores que 1
 while True:
     try:
@@ -13,13 +23,3 @@ while True:
 
 print("p_linear({}) = {}".format(N, p_linear(N))) # Imprime todos os números primos até o número N através da função linear
 print("p_recursivo({}) = {}".format(N, p_recursivo(N))) # Imprime todos os números primos até o número N através da função recursiva
-
-# Função que calcula todos os números primos até o número N de forma linear
-def p_linear(N):
-    # código
-    return
-
-# Função que calcula todos os números primos até o número N de forma recursiva
-def p_recursivo(N):
-    # código
-    return
