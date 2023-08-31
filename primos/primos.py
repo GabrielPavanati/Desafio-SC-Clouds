4e17603e564e76179462c575cbac721849f031d4
Gabriel Furtado Pavanati <gpavanati01@gmail.com>
2023-08-31 11:06:02 -0300

    add input and print to primos.py file

--- primos/primos.py
+++ primos/primos.py
@@ -1,1 +1,13 @@
+# coding: latin-1
 
+# Trata input para receber apenas números maiores que 1
+while True:
+    try:
+        print("Informe um número maior que 1: ")
+        N = int(input())
+
+    except:
+        continue
+    
+    if N > 1:
+        break
