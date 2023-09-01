3738764c59a604e988c04bd8faa77ea9280ef013
Gabriel Furtado Pavanati <gpavanati01@gmail.com>
2023-09-01 20:35:15 -0300

    add function p_linear()

--- primos/primos.py
+++ primos/primos.py
@@ -2,13 +2,27 @@
 
 # Função que calcula todos os números primos até o número N de forma linear
 def p_linear(N):
-    # código
-    return
+    memoria = []
+    
+    for i in range(2, N + 1):
+                
+        num_divisiveis = 0
+
+        for j in range(1, N + 1):
+            if i % j == 0:
+                num_divisiveis += 1
+                
+        if num_divisiveis == 2:
+            memoria.append(i)
+            
+    return memoria
+
+seq_recursiva = []
 
 # Função que calcula todos os números primos até o número N de forma recursiva
 def p_recursivo(N):
     # código
-    return
+    return 1
 
 # Trata input para receber apenas números maiores que 1
 while True:
@@ -21,5 +35,7 @@ while True:
     if N > 1:
         break
 
+print("\n")
 print("p_linear({}) = {}".format(N, p_linear(N))) # Imprime todos os números primos até o número N através da função linear
 print("p_recursivo({}) = {}".format(N, p_recursivo(N))) # Imprime todos os números primos até o número N através da função recursiva
+print("\n")
